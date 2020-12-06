d1df36aff042f42601c01780322fa95a34ce1569
Óscar Nájera <hi@oscarnajera.com>
2020-12-07 00:02:20 +0100

    Refactor extract

--- cal-sync.el
+++ cal-sync.el
@@ -10,38 +10,39 @@ which can be fed into `cal-sync-insert-org-entry'."
     (let* ((ical-list (icalendar--read-element nil nil))
            (zone-map (icalendar--convert-all-timezones ical-list)))
       (mapcar (lambda (event)
-                (cal-sync-create-org-entry event zone-map))
+                (cal-sync-enrich-properties event zone-map))
               (icalendar--all-events ical-list)))))
 
-(defun cal-sync-create-org-entry (event zone-map)
-  (defun get-prop (event property) ;; equivalent icalendar--get-event-property
-    (if-let ((value (alist-get property event)))
-        (cadr value)))
-  (defun get-attr (event property) ;; equivalent icalendar--get-event-property-attributes
-    (if-let ((value (alist-get property event)))
-        (car value)))
+(defun get-prop (event property) ;; equivalent icalendar--get-event-property
+  (if-let ((value (alist-get property event)))
+      (cadr value)))
 
-  (defun -ical-times (event-properties property &optional zone-map)
-    (icalendar--decode-isodatetime
-     (get-prop event-properties property)
-     nil
-     (icalendar--find-time-zone
-      (get-attr event-properties property)
-      zone-map)))
+(defun get-attr (event property) ;; equivalent icalendar--get-event-property-attributes
+  (if-let ((value (alist-get property event)))
+      (car value)))
 
-  (defun -ical-times-span (event &optional zone-map)
-    (let* ((dtstart-dec (-ical-times event 'DTSTART zone-map))
-	   (duration (get-prop event 'DURATION))
-	   (dtend-dec (-ical-times event 'DTEND zone-map)))
-      (when duration
-        (let ((dtend-dec-d (icalendar--add-decoded-times
-			    dtstart-dec
-			    (icalendar--decode-isoduration duration))))
-	  (when (and dtend-dec (not (eq dtend-dec dtend-dec-d)))
-	    (message "Inconsistent endtime and duration for %s" summary))
-	  (setq dtend-dec dtend-dec-d)))
-      `((START nil ,(encode-time dtstart-dec)) (END nil ,(encode-time dtend-dec)))))
+(defun -ical-times (event-properties property &optional zone-map)
+  (icalendar--decode-isodatetime
+   (get-prop event-properties property)
+   nil
+   (icalendar--find-time-zone
+    (get-attr event-properties property)
+    zone-map)))
 
+(defun -ical-times-span (event &optional zone-map)
+  (let* ((dtstart-dec (-ical-times event 'DTSTART zone-map))
+         (duration (get-prop event 'DURATION))
+         (dtend-dec (-ical-times event 'DTEND zone-map)))
+    (when duration
+      (let ((dtend-dec-d (icalendar--add-decoded-times
+                          dtstart-dec
+                          (icalendar--decode-isoduration duration))))
+        (when (and dtend-dec (not (eq dtend-dec dtend-dec-d)))
+          (message "Inconsistent endtime and duration for %s" summary))
+        (setq dtend-dec dtend-dec-d)))
+    `((START nil ,(encode-time dtstart-dec)) (END nil ,(encode-time dtend-dec)))))
+
+(defun cal-sync-enrich-properties (event zone-map)
   (let* ((event-properties (caddr event))
          (summary (icalendar--convert-string-for-import
                    (or (get-prop event-properties 'SUMMARY) "No Title"))))
@@ -96,10 +97,6 @@ which can be fed into `cal-sync-insert-org-entry'."
           (org-set-tags (split-string categories "[ ,]+"))))
 
     (buffer-string)))
-
-(md5 (buffer-substring-no-properties
-	  (org-entry-beginning-position)
-	  (org-entry-end-position)))
 
 (let* ((buf (find-file-noselect "/tmp/agenda-36afc42317"))
        (events (cal-sync-convert-event buf)))
